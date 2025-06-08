0d42f9c14890b9ab951de063d8de9b8091989583
skydtrtzmr <2368178395@qq.com>
2025-06-08 14:09:32 +0800

    Update index.ts

--- src/set-user-email-list-cache-hook/index.ts
+++ src/set-user-email-list-cache-hook/index.ts
@@ -4,7 +4,7 @@ import type {
     HookExtensionContext,
     RegisterFunctions,
 } from "@directus/extensions";
-import { updateListCache} from "../utils/redisUtils";
+import { updateListCache } from "../utils/redisUtils";
 
 const redis = new IORedis(process.env.REDIS!, {
     maxRetriesPerRequest: null, // 适用于 Redis 连接本身
@@ -15,38 +15,38 @@ export default defineHook(
         { init, schedule }: RegisterFunctions,
         hookContext: HookExtensionContext
     ) => {
-		const { services, getSchema, logger } = hookContext;
+        const { services, getSchema, logger } = hookContext;
         const { UsersService } = services;
-
 
         const cronSchedule =
             process.env.USER_PS_CACHE_CRON_SCHEDULE || "*/30 * * * *";
-			const INDEX_PREFIX = "user_email"; // Namespace for these keys
+        const INDEX_PREFIX = "user_email"; // Namespace for these keys
 
-		const fetchAndCacheUserEmailList = async() => {
-			const directusUserService = new UsersService({
-				schema: await getSchema(),
-			});
-			updateListCache(
-				redis,
-				"student_user_email_list",
-				async () =>
-					await directusUserService.readByQuery({
-							fields: ["email"],
-							sort: "email",
-							filter: {
-								role: {
-									name: {
-										_eq: "学生",
-									},
-								},
-							},
-							limit: -1,
-						}),
-				"email",
-				3600
-			);
-		}
+        const fetchAndCacheUserEmailList = async () => {
+            const directusUserService = new UsersService({
+                schema: await getSchema(),
+                accountability: { admin: true },
+            });
+            updateListCache(
+                redis,
+                "student_user_email_list",
+                async () =>
+                    await directusUserService.readByQuery({
+                        fields: ["email"],
+                        sort: ["email"], // 注意，sort一定是列表，否则会报错。
+                        filter: {
+                            role: {
+                                name: {
+                                    _eq: "学生",
+                                },
+                            },
+                        },
+                        limit: -1,
+                    }),
+                "email",
+                3600
+            );
+        };
 
         schedule(cronSchedule, async () => {
             logger.info(
